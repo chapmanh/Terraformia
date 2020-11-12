5de43b2bdb2ca73dc318cd2aaa875c0d1b770ec0
Haydn <haydn.chapman@decoded.com>
2020-11-12 20:02:57 +0000

    Liftoff!
    Background, Game, Player Folders defined, basic movement scripting for Player

---
+++ Moonshot/Player/Player.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+
+# Declare member variables here.
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+#	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+#		$AnimatedSprite.play() #Animation while moving only?
+#	else:
+#		$AnimatedSprite.stop() #Animation while moving only?
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	# Thrusters Animation, part of same godot scene? I think so...
+	
+	
+	# Code for animation while moving on the x/y axis respectively
+#	if velocity.x != 0: #walking left/right animation
+#		$AnimatedSprite.animation = "walk"
+#		#$AnimatedSprite.flip_v = false
+#		$AnimatedSprite.flip_h = velocity.x < 0
+#	elif velocity.y != 0:
+#		$AnimatedSprite.animation = "up"
+#		$AnimatedSprite.flip_v = velocity.y > 0

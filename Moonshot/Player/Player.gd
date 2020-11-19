2e3cd3afd214249b3ea5af693dbb9828847fcefb
Haydn <haydn.chapman@decoded.com>
2020-11-19 21:13:50 +0000

    Revamp Damage, include animation

--- Moonshot/Player/Player.gd
+++ Moonshot/Player/Player.gd
@@ -2,20 +2,33 @@ extends Area2D
 
 var plBullet := preload("res://Bullet/Bullet.tscn")
 
+signal health_updated(health)
+signal killed()
+
 #onready var animatedSprite := $AnimatedSprite
 
 onready var firingPositions := $FiringPositions
 onready var fireDelayTimer := $FireDelayTimer
-onready var invincibilityTimer := $InvincibilityTimer
+onready var invulnerabilityTimer := $InvulnerabilityTimer
+onready var effectAnimation := $EffectAnimation
 
 # Ship Damage Sprites
-onready var shipSprites := [$Ship_0, $Ship_1, $Ship_2, $Ship_3]
+onready var shipSprites := [
+	$Sprites/Ship_0,
+	$Sprites/Ship_1,
+	$Sprites/Ship_2,
+	$Sprites/Ship_3,
+	]
 
 export var speed: float = 100.0
 export var fireDelay: float = 0.1 
 export var invincibility: float = 2.0
-export var lifeMax: int = 4 # 0 = dead
-export var life: int = 4 # 0 = dead
+export var max_health: float = 4 # 0 = dead
+
+onready var health = max_health setget _set_health
+
+
+
 var vel := Vector2(0,0)
 
 func _ready() -> void:
@@ -42,11 +55,8 @@ func _process(delta):
 			get_tree().current_scene.add_child(bullet)
 	
 	# Collision detection: (returns false if array is empty)
-	if get_overlapping_areas() and invincibilityTimer.is_stopped():
-		invincibilityTimer.start()
-		print("Ow!")
+	if get_overlapping_areas():
 		damage(1)
-		print(life)
 	
 func _physics_process(delta: float) -> void:
 	var dirVec := Vector2(0,0)
@@ -72,18 +82,33 @@ func _physics_process(delta: float) -> void:
 	position.x = clamp(position.x, 0, viewRect.size.x)
 	position.y = clamp(position.y, 0, viewRect.size.y)
 
-
-
-func damage(amount: int): # amount not yet implemented, all hits = 1
-	life -= 1
-	if life <= 0:
-		die()
-	else:
-		var missingLife: int = lifeMax - life
-		shipSprites[missingLife].visible = true
-		shipSprites[missingLife -1].visible = false
 	
-	
+func damage(amount): # Could implement damage types here, with modifiers
+	if invulnerabilityTimer.is_stopped():	
+		invulnerabilityTimer.start()
+		_set_health(health - amount)
+		effectAnimation.play("damage")
+		effectAnimation.queue("flash")
 		
-func die():
+		
+	
+func kill():
 	queue_free()
+
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated", health)
+		if health == 0:
+			kill()
+			emit_signal("killed")
+		else:
+			var missingLife: int = max_health - health
+			shipSprites[missingLife].visible = true
+			shipSprites[missingLife -1].visible = false
+	
+
+
+func _on_InvulnerabilityTimer_timeout() -> void:
+	effectAnimation.play("rest")

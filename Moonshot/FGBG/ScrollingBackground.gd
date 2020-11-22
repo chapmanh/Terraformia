3a49f0e047332d11283f64293cc152ac092e40ad
Haydn <haydn.chapman@decoded.com>
2020-11-22 21:33:22 +0000

    tiny tidy up

--- Moonshot/FGBG/ScrollingBackground.gd
+++ Moonshot/FGBG/ScrollingBackground.gd
@@ -1,16 +1,8 @@
 extends TextureRect
 
-
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	material.set_shader_param("Scroll Speed", 0)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+	
+func _process(delta: float) -> void:
+	material.set_shader_param("Scroll Speed", 1)
+	print("dwa")

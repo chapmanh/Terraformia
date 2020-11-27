384160b937e79ae948da154673893bd0ee6b5c97
Haydn <haydn.chapman@decoded.com>
2020-11-27 19:29:11 +0000

    Seed Shrivel Mechanic (freezes after 0.5s)

--- Moonshot/Projectiles/Seed/Seed.gd
+++ Moonshot/Projectiles/Seed/Seed.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
 onready var animationPlayer = $AnimationPlayer
+onready var airTimer = $AirTimer
+onready var sprite = $Sprite
 
 export var speed: float = 500
 export var drag: float = 100
 
+func _ready() -> void:
+	airTimer.start()
+
 func _physics_process(delta: float) -> void:
 	position.y += speed * delta
 	position.x -= drag * delta
-
-
 
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
@@ -19,4 +22,10 @@ func _on_VisibilityNotifier2D_screen_exited() -> void:
 func _on_Seed_area_entered(area: Area2D) -> void:
 	if area.is_in_group("fertile"):
 		speed = 0
-		animationPlayer.play("grow")
+		if not airTimer.is_stopped():
+			animationPlayer.play("grow")
+			airTimer.queue_free() # Kill it, so no timeout trigger
+
+
+func _on_AirTimer_timeout() -> void:
+	animationPlayer.play("shrivel")

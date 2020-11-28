3f65d4adc03785519caff59d34c8dd0a60de0008
Haydn <haydn.chapman@decoded.com>
2020-11-28 15:19:46 +0000

    changed dict to array to enable sorting logic

--- Moonshot/MainScenes/HighScores.gd
+++ Moonshot/MainScenes/HighScores.gd
@@ -3,22 +3,40 @@ extends MarginContainer
 onready var labelNames = $ScoreBoard/VBoxContainer/HBoxContainer/LabelNames
 onready var labelScores = $ScoreBoard/VBoxContainer/HBoxContainer/LabelScores
 
-var scoreData = {
-	"Player 1 SUUUUPER LONG NAME TEST" : 1000,
-	"hfudiasfea" : 900,
-	"dhwuwiadhuwai" : 800,
-	"babx9oqoa": 700,
-	"hgiacjabchfda": 550,
-}
+var scoreData: Array = [
+	1000,
+	900,
+	800,
+	700,
+	550,
+]
+
+var nameData: Array = [
+	"Player 1 SUUUUPER LONG NAME TEST",
+	"hfudiasfea",
+	"dhwuwiadhuwai",
+	"babx9oqoa",
+	"hgiacjabchfda",
+]
+
+var board: Array = [nameData, scoreData]
+# board = [[name1, ...], [score1, ...]]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var names: String = ""
 	var scores: String = ""
 	
-	for name in scoreData:
-		names += str(name) + "\n"
-		scores += str(scoreData[name]) + "\n"
+	for i in range(len(scoreData)):
+		# names = board[0]
+		names += str(board[0][i]) + "\n"
+		# scores = board[1]
+		scores += str(board[1][i]) + "\n"
 	
 	labelNames.set_text(names)
 	labelScores.set_text(scores)
+
+# Given the current board, place attempt [attemptName, attemptScore] inside
+func _updateBoard(board, attempt):
+	pass
+	

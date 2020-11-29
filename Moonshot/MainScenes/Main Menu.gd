7c7dcb091a9eb4ef88c69a99ccb5b01e90d3168f
Haydn <haydn.chapman@decoded.com>
2020-11-29 19:50:43 +0000

    Intro Music + TweenOut

--- Moonshot/MainScenes/Main Menu.gd
+++ Moonshot/MainScenes/Main Menu.gd
@@ -1,10 +1,13 @@
 extends TextureRect
 
 onready var game = get_tree().current_scene.get_node("Game")
+onready var tweenOut = $AudioMainMenu/TweenOut
 
 func _on_New_Game_pressed() -> void:
 	$VBoxContainer/ButtonNewGame.disabled = true
 	$APMainMenu.play("fadeOut")
+	tweenOut.interpolate_property($AudioMainMenu, "volume_db", -12, -80, 4.0, 1, Tween.EASE_IN)
+	tweenOut.start()
 	game.new_game()
 	yield($APMainMenu, "animation_finished")
 	visible = false
